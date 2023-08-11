8d1b5a654eaa8501b3895821c5e9bd2770933436
so2emma <osoemmanuel1969@gmail.com>
2023-08-11 15:29:02 +0100

    setting up api resources

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\EventResource;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
@@ -13,7 +14,7 @@ class EventController extends Controller
      */
     public function index()
     {
-        return Event::all();
+        return EventResource::collection(Event::with('user')->get());
         // return 'hello world';
     }
 
@@ -32,7 +33,8 @@ class EventController extends Controller
             'user_id' => 1
         ]);
 
-        return $event;
+        $event->load('user');
+        return new EventResource($event);
     }
 
     /**
@@ -40,22 +42,34 @@ class EventController extends Controller
      */
     public function show(Event $event)
     {
-        return $event;
+        $event->load('user','attendees');
+        return new EventResource($event);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Event $event)
     {
-        //
+        $event->update(
+            $request->validate([
+            'name' => 'sometimes|string|max:255',
+            'description' => 'nullable|string',
+            'start_time' => 'sometomes|date',
+            'end_time' => 'sometomes|date|after:start_time',
+        ]));
+
+        $event->load('user');
+        return new EventResource($event);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Event $event)
     {
-        //
+        $event->delete();
+
+        return response(status: 204);
     }
 }
